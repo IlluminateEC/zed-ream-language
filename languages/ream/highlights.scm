6f2d8cc6549b07fb2f511df6dbdac7ff2066084b
Khaumi <51966695+CircuitButKerbin@users.noreply.github.com>
2026-09-13 01:34:04 -0500

    it would help if i updated highlights here too

--- languages/ream/highlights.scm
+++ languages/ream/highlights.scm
@@ -4,11 +4,19 @@
 "type" @keyword
 "|" @operator
 "&" @operator
+"=" @operator
+":" @operator
+"," @punctuation.delimiter
 "as" @keyword
 (atom) @constant
 (type_atom (tuple (type_composed) @type))
 (generic_application (identifier) @type)
 (type_definition_composed (identifier) @type)
 (generic_args_composed (identifier) @type)
-(map_pair (identifier) @field)
+(map_pair (identifier) @property)
 (import_composed (identifier) @module)
+(comment) @comment
+(doc_comment) @comment.doc
+(superdoc_comment) @comment.doc
+(function_definition_composed (identifier) @function)
+(function_arg (identifier) @variable.parameter)
